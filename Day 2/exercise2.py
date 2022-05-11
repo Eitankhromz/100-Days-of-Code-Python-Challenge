68cdfa481511548d18aae9fe0a50ee97868c4ba8
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-11 02:13:12 -0400

    Create exercise2.py

---
+++ Day 2/exercise2.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+#BMI = weight/(height ** 2)
+
+# Change weight and height data type from str to float
+int_height = float(height)
+int_weight = float(weight)
+
+#Complete BMI Calculation
+bmi = int_weight / int_height ** 2
+
+#		OR
+
+bmi = float(weight) / float(height) ** 2 
+print(int(bmi))
+
+# print bmi result
+print(int(bmi))
