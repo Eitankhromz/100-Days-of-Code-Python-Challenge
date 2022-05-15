0fcbff7dd3d71e14382b66d545b31983a5c4c210
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-15 01:06:33 -0400

    Create notes.py

---
+++ Day_6/notes.py
@@ -0,0 +1,26 @@
+print("hello word")
+num_char = len("Hello World")
+print(num_char)
+
+def my_function():
+  print("hello")
+  print("bye")
+
+my_function()
+#DEFINING FUNCTIONS:
+  # def function():
+      #Do this
+      #Then do this
+      #Finally do this
+
+#CALLING FUNCTIONS:
+  # function() 
+
+#WHILE LOOPS:
+    # while something_is_true:
+    #     do this
+
+#WHEN TO USE FOR VS WHILE LOOP:
+    #when you want to do something with each element use FOR
+
+    #when you dont care about each element use while
