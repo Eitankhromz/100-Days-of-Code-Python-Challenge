cb6801f3b6d60c8e80f32ac49501da37ec2484f3
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-15 01:05:38 -0400

    Update hurdle2.py

--- Day_6/hurdle2.py
+++ Day_6/hurdle2.py
@@ -15,3 +15,28 @@ def jump():
 
 for step in range (6):
     jump()
+
+#OR
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# steps = 0
+# while steps < 6:
+#     if at_goal():
+#         break
+#     else:
+#         jump()
+#         steps+=1
