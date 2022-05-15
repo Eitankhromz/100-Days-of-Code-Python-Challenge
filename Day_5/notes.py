46033d6b2fac4d0bdc07dab08c76f46d49ea8364
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-14 21:23:13 -0400

    Create notes.py

---
+++ Day_5/notes.py
@@ -0,0 +1,15 @@
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#   print(fruit) #will print as many times as for loop runs
+#   print(fruit + " Pie")
+# print(fruits) # will print only one time; outside of for loop
+
+#For Loop in range
+# for number in range(1, 10, 3):
+#   print(number)
+
+total = 0
+for number in range(1, 101):
+  total+=number
+
+print(total)
