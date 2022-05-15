a54308e0495b9d3b48768e5e349004c29d3770cf
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-15 01:01:18 -0400

    Create hurdle1.py

---
+++ Day_6/hurdle1.py
@@ -0,0 +1,20 @@
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def draw_square():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+
+draw_square()
