53531eb745ebbd5e7c80053033cc3ec101beb03e
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-15 01:02:01 -0400

    Create hurdle3.py

---
+++ Day_6/hurdle3.py
@@ -0,0 +1,22 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+steps = 0
+while steps < 6:
+    if at_goal():
+        break
+    else:
+        jump()
+        steps+=1
