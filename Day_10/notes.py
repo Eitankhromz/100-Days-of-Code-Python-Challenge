35ec33329fb9cc90e064ab51f77fff4c398d01c8
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-22 15:05:09 -0400

    Update notes.py

--- Day_10/notes.py
+++ Day_10/notes.py
@@ -1,1 +1,11 @@
-#Soon to be
+#Function with outputs
+
+def format_name(f_name, l_name):
+    """Take a first and last name and format it to return a title case version"""
+    if f_name == "" or l_name =="":
+        return "You didn't provide valid inputs"
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Return: {formated_f_name} {formated_l_name}"
+
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
