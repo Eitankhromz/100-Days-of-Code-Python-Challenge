8b21a9939e6606a7913868b44bda0a614c8a33d9
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-26 00:06:26 -0400

    Update and rename notes.py to exercise3.py

---
+++ Day_13/exercise3.py
@@ -0,0 +1,12 @@
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+    number += 1
+  elif number % 3 == 0:
+    print("Fizz")
+    number += 1
+  elif number % 5 == 0:
+    print("Buzz")
+    number += 1
+  else:
+    print(number)
