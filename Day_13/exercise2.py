5997809f54b51816ead3d14044975731e9ecb3d8
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-26 00:06:04 -0400

    Update exercise2.py

--- Day_13/exercise2.py
+++ Day_13/exercise2.py
@@ -1,1 +1,13 @@
-#soon
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
