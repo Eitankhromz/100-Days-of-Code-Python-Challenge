03cbeb4f1546db9a0e0b0a093c5c7c5d8c3105b8
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-31 00:17:01 -0400

    Update oop_coffee_machine.py

--- Day_16/oop_coffee_machine.py
+++ Day_16/oop_coffee_machine.py
@@ -2,20 +2,23 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-my_money_machine = MoneyMachine()
-my_CoffeeMaker = CoffeeMaker()
+# Create objects 
+# i.e. [object] = [class]
 my_menu = Menu()
+my_coffee_maker = CoffeeMaker()
+my_money_machine = MoneyMachine()
 
+# create var with boolean and loop thru the program
 machine_on = True
 while machine_on:
-    choice = input(f"What would you like? {my_menu.get_items()}: ")
+    choice = input(f"What would you like? ({my_menu.get_items()}): ")  # [object].[method()]
     if choice == "off":
         machine_on = False
     elif choice == "report":
-        my_CoffeeMaker.report()
+        my_coffee_maker.report()  # [object].[method()]
         my_money_machine.report()
     else:
         drink = my_menu.find_drink(choice)
-        if my_CoffeeMaker.is_resource_sufficient(drink):
+        if my_coffee_maker.is_resource_sufficient(drink):
             if my_money_machine.make_payment(drink.cost):
-                my_CoffeeMaker.make_coffee(drink)
+                my_coffee_maker.make_coffee(drink)
