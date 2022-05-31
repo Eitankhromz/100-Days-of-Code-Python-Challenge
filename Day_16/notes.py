19f6fac7be8107f5a47883695c3bb400afb0d26b
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-05-30 23:59:19 -0400

    Update notes.py

--- Day_16/notes.py
+++ Day_16/notes.py
@@ -1,1 +1,35 @@
-#IN PROGRESS
+# import another_module
+# print(another_module.another_variable)
+#
+# # import turtle
+# # timmy = turtle.Turtle()
+# #OR
+# # from [module] import [class], [class]
+# from turtle import Turtle, Screen
+# # object = class(a.k.a blueprint)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("blue")
+# timmy.forward(100)
+#
+#
+# # [object] = [class or blueprint]
+# my_screen = Screen()
+# #     [object].[attribute]
+# print(my_screen.canvheight)
+#
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"], "c")
+table.add_column("Type", ["Electric", "Water", "Fire"], "c")
+table.align = 'l'
+
+print(table.align)
+
+
+print(table)
+
