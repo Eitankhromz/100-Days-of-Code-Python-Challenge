0f518f89f3fbab3acf4dacfa13e6950736977c41
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-06-02 21:30:03 -0400

    Create etch-a-sketch.py

---
+++ Day_19/etch-a-sketch.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.fd(10)
+
+
+def move_backwards():
+    tim.back(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def clear_screen():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+
+screen.listen()
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkeypress(key="w", fun=move_forwards)
+screen.onkeypress(key="s", fun=move_backwards)
+screen.onkey(key="c", fun=clear_screen)
+
+screen.exitonclick()
