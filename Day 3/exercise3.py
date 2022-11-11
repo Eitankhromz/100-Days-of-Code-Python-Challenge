e953872a17ca3a34f43d25b26ba8f9df24472e77
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-11 14:00:16 -0500

    Update exercise3.py

--- Day 3/exercise3.py
+++ Day 3/exercise3.py
@@ -14,3 +14,30 @@ if year % 4 == 0:
         print("Leap year.")
 else:
     print("Not leap year.")
+
+ # 11/11 Solution
+
+#Write your code below this line 👇
+# if year % 4 != 0:
+#     print("Not a leap year.")
+# else:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap Year.")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year")
+        else:
+            print("Not leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year")
+    
+    
