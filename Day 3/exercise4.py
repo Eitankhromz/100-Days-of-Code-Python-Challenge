0b987ecf618f858d73baf0746f853e2332476bf5
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-11 21:52:07 -0500

    Update exercise4.py

--- Day 3/exercise4.py
+++ Day 3/exercise4.py
@@ -48,3 +48,28 @@ if extra_cheese == "Y":
     bill+=1
 
 print(f"Your final bill is: ${bill}.")
+
+OR
+
+bill = 0
+
+if size == 'S':
+    bill+=15
+    if add_pepperoni == 'Y':
+        bill+=2
+    if extra_cheese == 'Y':
+        bill+=1
+elif size == 'M':
+    bill+=20
+    if add_pepperoni == 'Y':
+        bill+=3
+    if extra_cheese == 'Y':
+        bill+=1
+else:
+    bill+=25
+    if add_pepperoni == 'Y':
+        bill+=3
+    if extra_cheese == 'Y':
+        bill+=1
+
+print(f"Your final bill is: ${bill}")
