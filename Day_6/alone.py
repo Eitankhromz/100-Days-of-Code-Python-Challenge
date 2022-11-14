19245a43b82ea0fade1a9fc20d1b51e58ada7eaa
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-14 09:54:11 -0500

    Create alone.py

---
+++ Day_6/alone.py
@@ -0,0 +1,18 @@
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+#Draw Square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
