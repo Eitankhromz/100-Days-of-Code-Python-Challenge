3391df3cb6007cb99a58e61fdd912a252f9f0957
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-14 10:30:13 -0500

    Update hurdle3.py

--- Day_6/hurdle3.py
+++ Day_6/hurdle3.py
@@ -20,3 +20,28 @@ while steps < 6:
     else:
         jump()
         steps+=1
+
+OR
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def hurdle():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while at_goal() != True:
+    hurdle()
+    
