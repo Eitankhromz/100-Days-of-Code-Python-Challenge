f71a42b06aedf005a403fbbc6bbe77a7cf6a73f2
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-14 21:53:12 -0500

    Update hurdle4.py

--- Day_6/hurdle4.py
+++ Day_6/hurdle4.py
@@ -16,3 +16,40 @@ while not at_goal():
         move()
     elif wall_in_front():
         turn_left()
+
+  OR
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def hurdle():
+    turn_left()
+    
+    while wall_on_right():
+        move()
+        
+    turn_right()
+    move()
+    turn_right()
+    
+    while front_is_clear():
+        move()
+    
+    turn_left()
+    
+    
+while not at_goal():
+    if wall_in_front():
+        hurdle()
+    else:
+        move()
+    
+    
+    
+    
