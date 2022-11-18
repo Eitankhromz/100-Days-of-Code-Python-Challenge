b73487601c795a5fc9a113f01ac227b4f7946a48
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-17 23:28:52 -0500

    Update exercise1.py

--- Day_9/exercise1.py
+++ Day_9/exercise1.py
@@ -25,6 +25,20 @@ for student in student_scores:
 print(student_grades)
     
 
+OR
 
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+  score = student_scores[student]
+  if score > 90:
+    student_grades[student] = "Outstanding"
+  elif score > 80:
+    student_grades[student] = "Exceeds Expectations"
+  elif score > 70:
+    student_grades[student] = "Acceptable"
+  else:
+    student_grades[student] = "Fail"
 
 
