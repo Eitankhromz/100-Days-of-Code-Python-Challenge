cb26043f47b021a988ed43b777cc4bd52f7401be
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-19 00:10:02 -0500

    Update notes.py

--- Day_10/notes.py
+++ Day_10/notes.py
@@ -9,3 +9,4 @@ def format_name(f_name, l_name):
     return f"Return: {formated_f_name} {formated_l_name}"
 
 print(format_name(input("What is your first name? "), input("What is your last name? ")))
+
