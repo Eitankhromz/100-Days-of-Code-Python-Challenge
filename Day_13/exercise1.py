76a2133750fd5b51f26a7162b65c4ad81c3fcef4
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-24 13:23:14 -0500

    Update exercise1.py

--- Day_13/exercise1.py
+++ Day_13/exercise1.py
@@ -1,6 +1,6 @@
 number = int(input("Which number do you want to check?"))
 
-if number % 2 == 0:
+if number % 2 == 0: #Missing two equal signs (checking)
   print("This is an even number.")
 else:
   print("This is an odd number.")
