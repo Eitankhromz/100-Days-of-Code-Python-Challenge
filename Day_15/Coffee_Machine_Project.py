1677d8a824f2425bf7b100b06784bb918964b891
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-27 22:14:50 -0500

    Update Coffee_Machine_Project.py

--- Day_15/Coffee_Machine_Project.py
+++ Day_15/Coffee_Machine_Project.py
@@ -129,3 +129,4 @@ def coffee_machine():
         ## The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer.
 
 coffee_machine()
+
