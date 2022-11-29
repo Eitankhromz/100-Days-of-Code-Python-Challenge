0276cb6a692b81ba11e07dff9d5fbf578dac1c65
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-28 21:21:18 -0500

    Update oop_coffee_machine.py

--- Day_16/oop_coffee_machine.py
+++ Day_16/oop_coffee_machine.py
@@ -22,3 +22,31 @@ while machine_on:
         if my_coffee_maker.is_resource_sufficient(drink):
             if my_money_machine.make_payment(drink.cost):
                 my_coffee_maker.make_coffee(drink)
+
+ #OR
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+#Usually you name object the snake case of the class
+coffee_menu = Menu()
+make_drink = CoffeeMaker()
+transaction = MoneyMachine()
+
+machine_on = True
+
+while machine_on:
+    user_order = input(f"What would you like? ({coffee_menu.get_items()}): ")
+
+    if user_order == "off":
+        machine_on = False
+    elif user_order == "report":
+        make_drink.report()
+        transaction.report()
+    else:
+        if coffee_menu.find_drink(user_order):
+            drink = coffee_menu.find_drink(user_order)
+            if make_drink.is_resource_sufficient(drink):
+                if transaction.make_payment(drink.cost):
+                    make_drink.make_coffee(drink)
