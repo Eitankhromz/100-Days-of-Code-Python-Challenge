2987c1e3bcfb6afe4ff9874eb4fa48ab05e9eaae
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-29 21:08:46 -0500

    Update question_model.py

--- Day_17/question_model.py
+++ Day_17/question_model.py
@@ -1,4 +1,4 @@
 class Question:
     def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
+        self.text = text #attribute
+        self.answer = answer #attribute
