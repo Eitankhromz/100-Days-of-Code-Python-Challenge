12ebb9f044834af28c8ef7cd4b3d300abbbe3fca
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-29 21:08:01 -0500

    Update main.py

--- Day_17/main.py
+++ Day_17/main.py
@@ -17,3 +17,25 @@ while quiz.still_has_questions():
 
 print("You've completed the quiz!")
 print(f"Your final score was {quiz.score}/{quiz.question_number}.")
+
+
+#OR
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    q_text = question["question"]
+    q_ans = question["correct_answer"]
+    new_question = Question(q_text, q_ans)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+
