61c9ade1338c056f100795c095fad0ed9d7dfd99
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-11-29 21:07:17 -0500

    Update data.py

--- Day_17/data.py
+++ Day_17/data.py
@@ -31,3 +31,58 @@ question_data = [
      "question": "The Python programming language gets its name from the British comedy group Monty Python",
      "correct_answer": "True", "incorrect_answers": ["False"]}
 ]
+##
+
+question_data = [
+    {
+                "category": "Vehicles", "type": "boolean", "difficulty": "medium",
+                "question": "The Japanese Shinkansen beat the French TGV&#039;s " "speed record for fastest electric "
+                            "rail train.",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]},
+                 {"category": "Science: Computers", "type": "boolean",
+                  "difficulty": "medium",
+                  "question": "&quot;Windows NT&quot; is a monolithic kernel.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Geography", "type": "boolean", "difficulty": "medium",
+                  "question": "Vietnam is the only country in the world that "
+                              "starts with V. ",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Entertainment: Video Games", "type": "boolean",
+                  "difficulty": "medium",
+                  "question": "In Half-Life 2, if you play the zombies&#039; "
+                              "speech in reverse, they actually speak coherent English.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Entertainment: Japanese Anime & Manga",
+                  "type": "boolean", "difficulty": "medium",
+                  "question": "The anime Attack on Titan was directed by Tetsur\u014d "
+                              "Araki, the same person who directed the anime Highschool"
+                              " of the Dead.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Science & Nature", "type": "boolean",
+                  "difficulty": "medium",
+                  "question": "Centripedal force is an apparent force that acts "
+                              "outward on a body moving around a center, arising from "
+                              "the body&#039;s inertia.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Entertainment: Video Games", "type": "boolean",
+                  "difficulty": "medium",
+                  "question": "David Baszucki was a co-founder of ROBLOX Corporation.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Entertainment: Television", "type": "boolean",
+                  "difficulty": "medium",
+                  "question": "An episode of &quot;The Simpsons&quot; is dedicated to "
+                              "Moe Szyslak&#039;s bar rag.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Entertainment: Video Games", "type": "boolean",
+                  "difficulty": "medium",
+                  "question": "In career mode of &quot;Need for Speed: Underground "
+                              "2&quot;, the first car the player can drive is "
+                              "the BMW M3 GTR.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Science & Nature", "type": "boolean",
+                  "difficulty": "medium",
+                  "question": "&quot;Tachycardia&quot; or &quot;Tachyarrhythmia&quot; "
+                              "refers to a resting heart-rate near or over 100 BPM.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]}]
+
