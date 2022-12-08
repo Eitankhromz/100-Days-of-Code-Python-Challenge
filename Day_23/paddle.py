7ebfe0ce1875bf34869176e84e13033222480e4b
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-12-07 23:59:18 -0500

    Create paddle.py

---
+++ Day_23/paddle.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.create_paddle(position)
+
+    def create_paddle(self, position):
+        self.color("white")
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
