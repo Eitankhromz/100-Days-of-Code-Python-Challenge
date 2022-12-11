159cecbde46f41df372bd85a8561040a3906a27a
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-12-11 18:30:29 -0500

    Create food.py

---
+++ Day_24/updated_snake_game/food.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+import random
+
+
+
+#class [class_name](super_class)
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color("blue")
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        x_cor = random.randint(-280, 280)
+        y_cor = random.randint(-280, 280)
+        self.goto(x=x_cor, y=y_cor)
