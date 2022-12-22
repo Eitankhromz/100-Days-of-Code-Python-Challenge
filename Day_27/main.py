8b862484863101de4481dd757c316679f916ad57
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-12-22 00:29:30 -0500

    Update main.py

--- Day_27/main.py
+++ Day_27/main.py
@@ -9,24 +9,30 @@ def button_clicked():
 window = Tk()
 window.title("My First GUI program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 #LABEL
 my_label = Label(text="I am a lablel", font=("Arial", 24, "bold"))
-my_label.pack(side="bottom", expand=True)
-
 #CHANGING TEXT
 my_label["text"] = "New Text"
-my_label.config(text="New TExt")
+my_label.config(text="New Text")
+# my_label.pack(side="bottom", expand=True)
+# my_label.place(x=0, y=0)
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# button.pack()
+button.grid(column=1, row=1)
 
 #ENTRY
 input = Entry(width=10)
-input.pack()
+# input.pack()
+input.grid(column=3, row=3)
 
-
+new_button = Button(text="I am a new button")
+new_button.grid(column=2, row=0)
 
 
 
