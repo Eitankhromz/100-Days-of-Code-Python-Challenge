c102a13ee7ec361c2a578aa307f5a63502f64dae
Eitankhromz <83232137+Eitankhromz@users.noreply.github.com>
2022-12-24 23:54:17 -0500

    Update exercise1.py

--- Day_30/exercise1.py
+++ Day_30/exercise1.py
@@ -4,9 +4,10 @@ fruits = ["Apple", "Pear", "Orange"]
 def make_pie(index):
     try:
         fruit = fruits[index]
-        print(fruit + " pie")
     except IndexError:
         print("Fruit pie")
+    else:
+        print(fruit + " pie")
 
 
 make_pie(4)
